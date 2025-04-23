a668d4381ad4faaea9e74e7cbc9be52c11afc2b0
vienit <vienchelsea325@gmail.com>
2025-04-23 21:57:08 +0700

    Update server.js

--- connectSV/server.js
+++ connectSV/server.js
@@ -21,6 +21,13 @@ fs.readdir(uploadDir, (err, files) => {
     }
 });
 
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
 // Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/quantri', quanTri);
